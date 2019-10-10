015dae663b73adbb9496ca1c1fa236ed824567c1
fakhri ramdani <muhammadlukman09@gmail.com>
2019-10-10 21:26:03 +0700

    Project keduapuluhenam

---
+++ src/pboul/pkg10115229/latihan26/waktusaatini/PBOUL10115229Latihan26WaktuSaatIni.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboul.pkg10115229.latihan26.waktusaatini;
+
+import java.util.Date;
+
+
+
+/**
+ *
+ * @author Lukman
+ * NIM      : 10115229   
+ * NAMA     : MUHAMMAD LUKMAN HAKIM
+ * KELAS    : PBO ULANG
+ */
+public class PBOUL10115229Latihan26WaktuSaatIni {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Date date = new Date();
+        
+        System.out.println("Hari ini adalah hari : " +date.toString());
+        System.out.println("");
+        System.out.println(" Develope By : Muhammad Lukman ");
+        
+    }
+    
+}
